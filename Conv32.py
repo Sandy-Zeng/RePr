2e459e6ebd0f56f842092700811dd587ebb587ae
Sandy-Zeng <784557382@qq.com>
2019-04-01 10:07:43 +0800

    change the reinitialize

--- Conv32.py
+++ Conv32.py
@@ -8,12 +8,12 @@ import numpy as np
 from scipy.linalg import null_space
 from scipy import linalg
 import os
-
-
 from summaries import TensorboardSummary
 
-summary = TensorboardSummary('./repr_dir/test6')
-tb_writer = summary.create_summary()
+summary = TensorboardSummary('./repr_dir/test4/net_1')
+net1_writer = summary.create_summary()
+test_summary = TensorboardSummary('./repr_dir/test4/net_2')
+net2_writer = test_summary.create_summary()
 
 
 class Net(nn.Module):
@@ -25,8 +25,13 @@ class Net(nn.Module):
         # self.avgpool = nn.AvgPool2d(32)
         self.fc = nn.Linear(32 * 32 * 32, 10)
 
-    def forward(self, x):
+    def forward(self, x, spasity, mode):
         # input shape: (b, 3, 32, 32)
+        # if mode == 'sub-network':
+        #     res = F.relu(1.0/spasity[0] * self.conv1(x))
+        #     res = F.relu(1.0 / spasity[1] * self.conv2(res))
+        #     res = F.relu(1.0 / spasity[2] * self.conv3(res))
+        # else:
         res = F.relu(self.conv1(x))
         res = F.relu(self.conv2(res))
         res = F.relu(self.conv3(res))
@@ -45,8 +50,8 @@ def prepare_data():
     normalize = transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 
     transform_train = transforms.Compose([
-        transforms.RandomCrop(32, padding=4),
-        transforms.RandomHorizontalFlip(),
+        # transforms.RandomCrop(32, padding=4),
+        # transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010])
     ])
@@ -57,14 +62,14 @@ def prepare_data():
 
     kwargs = {'pin_memory': True}
 
-    trainset = datasets.CIFAR10('./data/', train=True, download=False, transform=transform_train)
-    testset = datasets.CIFAR10('./data/', train=False, transform=transform_test)
-    train_loader = torch.utils.data.DataLoader(trainset, batch_size=32, shuffle=True, **kwargs)
-    val_loader = torch.utils.data.DataLoader(testset, batch_size=32, shuffle=False, **kwargs)
+    trainset = datasets.CIFAR10('../data/CIFAR10', train=True, download=True, transform=transform_train)
+    testset = datasets.CIFAR10('../data/CIFAR10', train=False, transform=transform_test)
+    train_loader = torch.utils.data.DataLoader(trainset, batch_size=8, shuffle=True, **kwargs)
+    val_loader = torch.utils.data.DataLoader(testset, batch_size=8, shuffle=False, **kwargs)
 
     return train_loader, val_loader
 
-def train(epoch, training_type):
+def train(epoch, training_type,spasity):
     # scheduler1.step()
     # scheduler2.step()
     corrected1 = 0
@@ -77,8 +82,8 @@ def train(epoch, training_type):
 
         input = input.to(device)
         target = target.to(device)
-        res1 = net1(input)
-        res2 = net2(input)
+        res1 = net1(input,spasity=spasity,mode='full-network')
+        res2 = net2(input, spasity=spasity, mode=training_type)
 
         loss1 = criterion(res1, target)
         loss2 = criterion(res2, target)
@@ -107,12 +112,12 @@ def train(epoch, training_type):
     acc1 = corrected1.item() / total
     acc2 = corrected2.item() / total
     log(log_file, "Training, Standard Accuracy: {}, RePr Accuracy: {}".format(acc1, acc2))
-    tb_writer.add_scalar('net1 train acc',acc1,epoch)
-    tb_writer.add_scalar('net2 train acc',acc2,epoch)
+    net1_writer.add_scalar('train acc',acc1,epoch)
+    net2_writer.add_scalar('train acc',acc2,epoch)
 
 net1_val_acc = 0.
 net2_val_acc = 0.
-def test():
+def test(training_type,spasity):
     global net1_val_acc,net2_val_acc
     corrected1 = 0
     corrected2 = 0
@@ -120,8 +125,10 @@ def test():
     for i, (input, target) in enumerate(val_loader):
         input = input.to(device)
         target = target.to(device)
-        res1 = net1(input)
-        res2 = net2(input)
+        res1 = net1(input, spasity=spasity, mode='full-network')
+        res2 = net2(input, spasity=spasity, mode=training_type)
+        # res1 = net1(input)
+        # res2 = net2(input)
 
         total += input.size(0)
 
@@ -134,8 +141,8 @@ def test():
     acc2 = corrected2.item() / total
     log(log_file, "Testing , Standard Accuracy: {}, RePr Accuracy: {}".format(acc1, acc2))
     log(log_file, "\n")
-    tb_writer.add_scalar('net1 val acc', acc1, epoch)
-    tb_writer.add_scalar('net2 val acc', acc2, epoch)
+    net1_writer.add_scalar('val acc', acc1, epoch)
+    net2_writer.add_scalar('val acc', acc2, epoch)
     if acc1 >net1_val_acc:
         net1_val_acc = acc1
     if acc2 > net2_val_acc:
@@ -151,7 +158,7 @@ def ortho(conv_layer):
     P = torch.abs(
         torch.matmul(normalized_conv_layer, normalized_conv_layer.transpose(0, 1)) - torch.eye(c_out).to(device))
     assert (P.shape == (c_out, c_out))
-    rank = torch.sum(P, dim=0) / c_out
+    rank = torch.sum(P, dim=1) / c_out
     return rank.view(c_out, 1)
 
 
@@ -181,9 +188,16 @@ def cal_dropped_filters(p):
     for layer, weight_list in dropped_filters_weight.items():
         dropped_filters_weight[layer] = torch.cat([w.flatten().unsqueeze(0) for w in weight_list], dim=0)
 
+    spasity = [1.0,1.0,1.0]
     for layer_index, filter_list in dropped_filters.items():
         print ('layer %d drop %d filter' % (layer_index, len(filter_list)))
-    return dropped_filters, dropped_filters_weight
+        if layer_index == 0:
+            spasity[0] = len(filter_list)/32.0
+        if layer_index == 2:
+            spasity[1] = len(filter_list)/32.0
+        if layer_index == 4:
+            spasity[2] = len(filter_list)/32.0
+    return dropped_filters, dropped_filters_weight,spasity
 
 
 def drop():
@@ -222,10 +236,47 @@ def re_initiate_zyy():
         W = W.cpu().numpy()
 
         if len(undropped_filter_list) != 0:
-            q, r = linalg.qr(W.T, mode='economic')
-            M, N = q.shape
+            q, r = linalg.qr(W, mode='full')
+            M, N = r.shape
+            # print(M,N)
             K = len(dropped_filter_list)
-            new_ortho_filter = torch.FloatTensor((q.dot(torch.eye(N, K))).T)
+            # new_ortho_filter = torch.FloatTensor((q.dot(torch.eye(N, K))).T)
+            if M >= N:
+                if len(undropped_filter_list) < N:
+                    # print(undropped_filters.shape)
+                    q1,r1 = linalg.qr(undropped_filters.cpu().numpy(),mode='full')
+                    # print(r1.shape)
+                    # print(K)
+                    new_ortho_filter = torch.from_numpy((null_space(r1)).T)
+                    print(torch.matmul(undropped_filters,torch.transpose(new_ortho_filter.cuda(),0,1)))
+                else:
+                    ranks = ortho(undropped_filters)
+                    ranks = [(i, ranks[i]) for i in range(ranks.shape[0])]
+                    ranks.sort(key=lambda x: x[1])
+                    ranks = ranks[-N+1:]
+                    select_filter = [r[0] for r in ranks]
+                    q1, r1 = linalg.qr(undropped_filters[select_filter].cpu().numpy(), mode='full')
+                    new_ortho_filter = torch.from_numpy((null_space(r1)).T)
+                    print(torch.matmul(undropped_filters, torch.transpose(new_ortho_filter.cuda(), 0, 1)))
+            else:
+                new_ortho_filter = torch.from_numpy((null_space(r)).T)
+                print(torch.matmul(undropped_filters, torch.transpose(new_ortho_filter.cuda(), 0, 1)))
+
+            if new_ortho_filter.shape[0] < K:
+                K1,K2 = new_ortho_filter.shape
+                zero_pad = torch.zeros((K-K1,K2))
+                new_ortho_filter = torch.cat([new_ortho_filter,zero_pad],dim=0)
+            if new_ortho_filter.shape[0] > K:
+                new_ortho_filter = new_ortho_filter[:K]
+            # print(new_ortho_filter.shape)
+            maxValue = torch.max(undropped_filters)
+            minValue = torch.min(undropped_filters)
+            new_ortho_filter = torch.clamp(new_ortho_filter,min=minValue,max=maxValue).cuda()
+            new_ortho_filter = (torch.mean(undropped_filters)/torch.mean(new_ortho_filter)) * new_ortho_filter
+            print (torch.mean(undropped_filters))
+            print (torch.mean(new_ortho_filter))
+            # print ('-----------------')
+            # print (new_ortho_filter)
             torch.nn.init.constant(param[layer_index][dropped_filter_list], 0)
             param[layer_index][dropped_filter_list].data += new_ortho_filter.view(K, c_in, k_w, k_h).float().cuda()
         else:
@@ -306,19 +357,20 @@ while epoch < 109:
     for e in range(S1):
         if epoch > 109:
             break
-        train(epoch, 'full network')
-        test()
+        spasity = [1.0,1.0,1.0]
+        train(epoch, 'full network',spasity)
+        test('full network',spasity)
         epoch += 1
 
-    dropped_filters, dropped_filters_weight = cal_dropped_filters(p)
+    dropped_filters, dropped_filters_weight,spasity = cal_dropped_filters(p)
     drop()
 
     for e in range(S2):
         # drop()
         if epoch > 109:
             break
-        train(epoch, 'sub-network')
-        test()
+        train(epoch, 'sub-network',spasity)
+        test('sub-network',spasity)
         epoch += 1
 
     re_initiate_zyy()
